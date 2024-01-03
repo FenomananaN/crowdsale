d51db774889fc8af630f592109496edad75fe5bb
FenomananaN <fenomanana.nomenjanahary@gmail.com>
2024-01-03 14:32:55 +0300

    featt switch chain

--- src/component/smartContract/ConnectWallet.jsx
+++ src/component/smartContract/ConnectWallet.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
-import { ConnectWallet } from "@thirdweb-dev/react";
+import React ,{ useEffect } from 'react'
+import {
+  useNetworkMismatch,
+  useNetwork,
+  ConnectWallet,
+  useAddress,
+  useConnectionStatus
+  } from "@thirdweb-dev/react";
+import { currentChainId } from '../../contract';
+
 
 export const ConnectWalletButton = () => {
+
+  const address = useAddress(); // Get connected wallet address
+  const [, switchNetwork] = useNetwork(); // Switch to desired chain
+  const isMismatched = useNetworkMismatch(); // Detect if user is connected to the wrong network
+
+
+  useEffect(() => {
+    if (isMismatched) {
+      switchNetwork(currentChainId)
+    }
+  }, [address]); 
+
   return (
     <ConnectWallet
-    theme={"dark"}
-    modalTitle={"Choisir Wallet"}
-    modalSize={"compact"}
-    welcomeScreen={{ title: "aas" }}
-    modalTitleIconUrl={""}
-    style={{backgroundColor: '#0B5E8F', color:'#fff', marginLeft:3}}
-/>
+      theme={"dark"}
+      modalTitle={"Choisir Wallet"}
+      modalSize={"compact"}
+      welcomeScreen={{ title: "aas" }}
+      modalTitleIconUrl={""}
+      style={{
+        backgroundColor: '#0B5E8F', 
+        color:'#fff', 
+        marginLeft:3, 
+        paddingTop:address ? 3 : {xs:0,md:''},
+        paddingBottom:address ? 3 : {xs:0,md:''},
+        }}
+    />
   )
 }
+
--- src/contract/contract.js
+++ src/contract/contract.js
@@ -1,6 +1,7 @@
 /*export const tokenAddress ='0xc1950A9B1715B6Ef749B409dDdA3d6E973E0e2ca'
 export const crowdsaleAddress ='0x7a68e823AaEE9AE070EA3D896d1Cc9979507534B'
 export const tetherAddress ='0x76a41272B137C4A440b4eF83feAe6972cd79e6c7'
+export const currentChainId = 
 */
 
 
@@ -8,6 +9,7 @@ export const tetherAddress ='0x76a41272B137C4A440b4eF83feAe6972cd79e6c7'
 export const tokenAddress ='0x0F883A270310519F1f331f1824E9639d73440d57'
 export const crowdsaleAddress ='0x75E035C332D99462a13aD50ef53BA2aF851d3F61'
 export const tetherAddress ='0x900101d06A7426441Ae63e9AB3B9b0F63Be145F1'
+export const currentChainId = 1116
 
 
 /*
@@ -15,4 +17,5 @@ export const tetherAddress ='0x900101d06A7426441Ae63e9AB3B9b0F63Be145F1'
 export const tokenAddress ='0x0F883A270310519F1f331f1824E9639d73440d57'
 export const crowdsaleAddress ='0x4b4013E9d52a7749Fa0D73DaD16AEED7012cfB55'
 export const tetherAddress = '0x3786495F5d8a83B7bacD78E2A0c61ca20722Cce3'
+export const currentChainId = 1115
 */
