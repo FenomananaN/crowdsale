5b94a20efbd6a1b8ed41175eb71cab80b41dfd25
FenomananaN <fenomanana.nomenjanahary@gmail.com>
2024-02-29 11:22:00 +0300

    new airdrop

--- src/scene/Airdrop.jsx
+++ src/scene/Airdrop.jsx
@@ -12,24 +12,22 @@ export const Airdrop = ({id}) => {
             border: '2px solid #C2992D'
         }}>
             <Typography variant='h5' align='center' sx={{color: '#C2992D', fontWeight: 'bold'}}>AIRDROP</Typography>
-            <Typography variant='h6' align='center' sx={{fontWeight: 'bold', mt:2}}>Join the Bitjoy Airdrop: Be Part of the Action!</Typography>
-            <Typography align='center' sx={{fontSize: 18, my:2}}>Are you ready for exciting rewards and opportunities? Dive into a unique crypto experience by joining our Bitjoy Airdrop today!</Typography>
-            <Typography variant='h6' align='center' fontWeight={'bold'}>🚀 How to Participate?</Typography>
-            <Typography align='center' sx={{fontSize: 18}} py={1}>It's simple! Visit our official website and sign up for the Airdrop in just a few clicks.</Typography>
+            <Typography variant='h6' align='center' sx={{fontWeight: 'bold', mt:2}}>🎉 Join BitJoy's Airdrop! 🚀</Typography>
+            <Typography align='center' sx={{fontSize: 18, my:2}}>🌟 Ready for an adventure? BitJoy invites you to join our Telegram community and earn rewards daily!</Typography>
+            <Typography variant='h6' align='center' fontWeight={'bold'}>💰 Participate in our airdrop</Typography>
+            <Typography align='center' sx={{fontSize: 18}} py={1}>Under 1000 members: Get 20 BITJOY/day.
+              Over 1000 members: Rewards increase!
+              Surpass 100,000 members: Earn up to 100 BITJOY/day!</Typography>
             
-            <Typography variant='h6' align='center' fontWeight={'bold'}>💰 Exclusive Rewards</Typography>
-            <Typography align='center' sx={{fontSize: 18}} py={1}>By participating, you can earn Bitjoy Points, our very own digital currency, redeemable for amazing prizes like $BNB or $BITJOY tokens. The more you participate, the greater your chances of winning!</Typography>
+            <Typography align='center' sx={{fontSize: 18}} py={1}>🤖 Claim rewards via our Telegram bot once you're in!</Typography>
             
-            <Typography variant='h6' align='center' fontWeight={'bold'}>🎁 Referral Bonus</Typography>
-            <Typography align='center' sx={{fontSize: 18}} py={1}>Invite your friends to join and earn even more rewards! Each new member you refer earns you additional points.</Typography>
-            
-            <Typography align='center' sx={{fontSize: 18}} py={1}>Don't miss out on this opportunity to join a vibrant community, earn exclusive rewards, and embark on an exciting</Typography>
 
             <Box sx={{display:'flex' , justifyContent:'center', my:2}}>
               <Button variant='contained' color='main' href='https://t.me/BitjoyTestButtons_bot?start=Bot11487633' target='_blank' rel='noopener noreferrer'>Join the Airdrop Now!</Button>
             </Box>
-            <Typography align='center' sx={{fontSize: 18}} py={1}>Stay tuned for more updates and exciting announcements!</Typography>
+            <Typography align='center' sx={{fontSize: 18}} py={1}>📢 Invite friends & let's grow together!</Typography>
             
+            <Typography align='center' sx={{fontSize: 18}} py={1}>Don't miss out on this opportunity. Join BitJoy today and start earning!</Typography>
         </Box>
     </Container>
   )
---
+++ src/sceneAdmin/Contirbutor.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Contirbutor = () => {
+  return (
+    <div>Contirbutor</div>
+  )
+}
